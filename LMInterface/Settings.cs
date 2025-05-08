3cd976c8559526702f71b9af4484c3908fc5caa0
Felix <f.schulze.2003@web.de>
2025-05-09 01:36:25 +0200

    Changed to nullable.

--- LMInterface/Settings.cs
+++ LMInterface/Settings.cs
@@ -5,7 +5,7 @@ namespace LMInterface
     public class Settings {
 
         //API stuff
-        [JsonProperty("api_url")] public string ApiUrl { get; set; }
-        [JsonProperty("selected_model")] public string SelectedModel { get; set; }
+        [JsonProperty("api_url")] public string? ApiUrl { get; set; } = "";
+        [JsonProperty("selected_model")] public string? SelectedModel { get; set; } = "";
     }
 }
