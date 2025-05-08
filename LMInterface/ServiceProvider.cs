81dfcf792b7034eefab4e43775db18167a101fc3
Felix <f.schulze.2003@web.de>
2025-05-09 01:38:26 +0200

    ServiceProvider now allows for loading/saving of services.

--- LMInterface/ServiceProvider.cs
+++ LMInterface/ServiceProvider.cs
@@ -1,8 +1,39 @@
-﻿namespace LMInterface
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using CommunityToolkit.WinUI.Helpers;
+using Newtonsoft.Json;
+
+namespace LMInterface
 {
     public static class ServiceProvider {
 
-        public static Settings Settings { get; } = new ();
+        public static Settings Settings { get; private set; } = new ();
 
+        public static async Task LoadSaves() {
+            Settings = await LoadService<Settings>();
+        }
+
+        public static async Task SaveServices() {
+            await SaveService(Settings);
+        }
+
+        private static async Task<T> LoadService<T>() where T : new() {
+            //if file doesnt exists, create new service
+            if (!await ApplicationData.Current.LocalFolder.FileExistsAsync($"{typeof(T).Name}.json")) return new T();
+
+            //read and deserialize file
+            var file = await ApplicationData.Current.LocalFolder.GetFileAsync($"{typeof(T).Name}.json");
+            var json = await FileIO.ReadTextAsync(file);
+            return JsonConvert.DeserializeObject<T>(json) ?? new T();
+        }
+
+        private static async Task SaveService<T>(T service) {
+            //create/overwrite file with name of type
+            var file = await ApplicationData.Current.LocalFolder.CreateFileAsync($"{typeof(T).Name}.json", CreationCollisionOption.ReplaceExisting);
+
+            var json = JsonConvert.SerializeObject(service);
+            await FileIO.WriteTextAsync(file, json);
+        }
     }
 }
